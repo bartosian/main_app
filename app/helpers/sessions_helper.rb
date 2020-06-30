c01ffe2ab711a5c111c534ee8d96220aebb271fd
bartosian <bartosian1989@gmail.com>
2020-06-30 14:49:50 -0400

    Added persistent session cookie

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,12 @@ module SessionsHelper
 
   end
 
+  def remember(user)
+	user.remember
+	cookies.permanent.signed[:user_id] = user.id
+	cookies.permanent[:remember_token] = user.remember_token
+  end
+
 
 
   # Returns the current logged-in user (if any).
