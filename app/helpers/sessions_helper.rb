3335306c1328667fdd90d8ef78d4c4eacfbd3c9c
bartosian <bartosian1989@gmail.com>
2020-07-14 21:30:23 -0400

    Added ability for updating users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,7 +16,9 @@ module SessionsHelper
 	cookies.permanent[:remember_token] = user.remember_token
   end
 
-
+  def current_user?(user)
+	user == current_user
+  end
 
   # Returns the current logged-in user (if any).
 
