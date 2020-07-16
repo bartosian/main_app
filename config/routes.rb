8623d563797bac1701e95491cb2d85fc366d5bd5
bartosian <bartosian1989@gmail.com>
2020-07-16 19:35:02 -0400

    Fixed tests syntax

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,8 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :account_activations, only: [:edit]
+
   root 'static_pages#home'
 end
